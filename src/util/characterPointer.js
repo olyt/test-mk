9c6bad840b323268348e3ebba2624d3fb7cef92e
Oleksandr Lytovchenko <olyt54@gmail.com>
2023-03-23 12:48:13 +0200

    add characters assets

---
+++ src/util/characterPointer.js
@@ -0,0 +1,64 @@
+const characterPointer = [
+    {
+        icon: '/img/characters/icons/cyrax.gif',
+        presentation: '/img/characters/presentations/cyrax.gif'
+    },
+    {
+        icon: '/img/characters/icons/ermac.gif',
+        presentation: '/img/characters/presentations/ermac.png'
+    },
+    {
+        icon: '/img/characters/icons/jade.gif',
+        presentation: '/img/characters/presentations/jade.gif'
+    },
+    {
+        icon: '/img/characters/icons/jax.gif',
+        presentation: '/img/characters/presentations/jax.gif'
+    },
+    {
+        icon: '/img/characters/icons/kabal.gif',
+        presentation: '/img/characters/presentations/kabal.gif'
+    },
+    {
+        icon: '/img/characters/icons/kano.gif',
+        presentation: '/img/characters/presentations/kano.gif'
+    },
+    {
+        icon: '/img/characters/icons/kunglao.gif',
+        presentation: '/img/characters/presentations/kunglao.gif'
+    },
+    {
+        icon: '/img/characters/icons/mileena.gif',
+        presentation: '/img/characters/presentations/mileena.gif'
+    },
+    {
+        icon: '/img/characters/icons/nightwolf.gif',
+        presentation: '/img/characters/presentations/nightwolf.gif'
+    },
+    {
+        icon: '/img/characters/icons/reptile.gif',
+        presentation: '/img/characters/presentations/reptile.gif'
+    },
+    {
+        icon: '/img/characters/icons/sektor.gif',
+        presentation: '/img/characters/presentations/sektor.gif'
+    },
+    {
+        icon: '/img/characters/icons/sindel.gif',
+        presentation: '/img/characters/presentations/sindel.gif'
+    },
+    {
+        icon: '/img/characters/icons/sonya.gif',
+        presentation: '/img/characters/presentations/sonya.gif'
+    },
+    {
+        icon: '/img/characters/icons/stryker.gif',
+        presentation: '/img/characters/presentations/stryker.gif'
+    },
+    {
+        icon: '/img/characters/icons/subzero.gif',
+        presentation: '/img/characters/presentations/subzero.gif'
+    },
+];
+
+export default characterPointer;

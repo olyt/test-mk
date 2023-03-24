aca9b73bed4eac3b29179f1405e7e75812384cd6
Oleksandr Lytovchenko <olyt54@gmail.com>
2023-03-24 11:26:58 +0200

    update paths to versus images

--- src/util/characterPointer.js
+++ src/util/characterPointer.js
@@ -1,63 +1,78 @@
 const characterPointer = [
     {
         icon: '/img/characters/icons/cyrax.gif',
-        presentation: '/img/characters/presentations/cyrax.gif'
+        presentation: '/img/characters/presentations/cyrax.gif',
+        versus: '/img/characters/presentations/cyrax.png'
     },
     {
         icon: '/img/characters/icons/ermac.gif',
-        presentation: '/img/characters/presentations/ermac.png'
+        presentation: '/img/characters/presentations/ermac.png',
+        versus: '/img/characters/presentations/ermac.png'
     },
     {
         icon: '/img/characters/icons/jade.gif',
-        presentation: '/img/characters/presentations/jade.gif'
+        presentation: '/img/characters/presentations/jade.gif',
+        versus: '/img/characters/presentations/jade.png'
     },
     {
         icon: '/img/characters/icons/jax.gif',
-        presentation: '/img/characters/presentations/jax.gif'
+        presentation: '/img/characters/presentations/jax.gif',
+        versus: '/img/characters/presentations/jax.png'
     },
     {
         icon: '/img/characters/icons/kabal.gif',
-        presentation: '/img/characters/presentations/kabal.gif'
+        presentation: '/img/characters/presentations/kabal.gif',
+        versus: '/img/characters/presentations/kabal.png'
     },
     {
         icon: '/img/characters/icons/kano.gif',
-        presentation: '/img/characters/presentations/kano.gif'
+        presentation: '/img/characters/presentations/kano.gif',
+        versus: '/img/characters/presentations/kano.png'
     },
     {
         icon: '/img/characters/icons/kunglao.gif',
-        presentation: '/img/characters/presentations/kunglao.gif'
+        presentation: '/img/characters/presentations/kunglao.gif',
+        versus: '/img/characters/presentations/kunglao.png'
     },
     {
         icon: '/img/characters/icons/mileena.gif',
-        presentation: '/img/characters/presentations/mileena.gif'
+        presentation: '/img/characters/presentations/mileena.gif',
+        versus: '/img/characters/presentations/mileena.png'
     },
     {
         icon: '/img/characters/icons/nightwolf.gif',
-        presentation: '/img/characters/presentations/nightwolf.gif'
+        presentation: '/img/characters/presentations/nightwolf.gif',
+        versus: '/img/characters/presentations/nightwolf.png'
     },
     {
         icon: '/img/characters/icons/reptile.gif',
-        presentation: '/img/characters/presentations/reptile.gif'
+        presentation: '/img/characters/presentations/reptile.gif',
+        versus: '/img/characters/presentations/reptile.png'
     },
     {
         icon: '/img/characters/icons/sektor.gif',
-        presentation: '/img/characters/presentations/sektor.gif'
+        presentation: '/img/characters/presentations/sektor.gif',
+        versus: '/img/characters/presentations/sektor.png'
     },
     {
         icon: '/img/characters/icons/sindel.gif',
-        presentation: '/img/characters/presentations/sindel.gif'
+        presentation: '/img/characters/presentations/sindel.gif',
+        versus: '/img/characters/presentations/sindel.png'
     },
     {
         icon: '/img/characters/icons/sonya.gif',
-        presentation: '/img/characters/presentations/sonya.gif'
+        presentation: '/img/characters/presentations/sonya.gif',
+        versus: '/img/characters/presentations/sonya.png'
     },
     {
         icon: '/img/characters/icons/stryker.gif',
-        presentation: '/img/characters/presentations/stryker.gif'
+        presentation: '/img/characters/presentations/stryker.gif',
+        versus: '/img/characters/presentations/stryker.png'
     },
     {
         icon: '/img/characters/icons/subzero.gif',
-        presentation: '/img/characters/presentations/subzero.png'
+        presentation: '/img/characters/presentations/subzero.png',
+        versus: '/img/characters/presentations/subzero.png'
     },
 ];
 
